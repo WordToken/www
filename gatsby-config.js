e4612cb718a283cd091b5f82729a1b54064770c2
Lemuel Uhuru <wish@devgenie.com>
2020-03-08 11:31:47 -0400

    add markdown to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,15 +1,41 @@
 import React from "react"
+import { graphql } from 'gatsby'
 import { Link } from "gatsby"
 
 import Layout from "../../components/layout"
 import Image from "../../components/image"
 import SEO from "../../components/seo"
 
-const BlogIndexPage = () => (
-  <Layout>
+const BlogIndexPage = ({ data }) => {
+  const { edges } = data.allMarkdownRemark
+
+  return (<Layout>
     <SEO title="Blog" />
     <h1>Blog</h1>
-  </Layout>
-)
+    <ul>{edges.map(edge => {
+      const { frontmatter } = edge.node
+      return <li><Link to={frontmatter.path}>{frontmatter.title}</Link></li>
+    })}</ul>
+  </Layout>)
+}
+
+
+export const query = graphql`
+  query BlogIndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
+
 
 export default BlogIndexPage
